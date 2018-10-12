3456b40ceee5310221f40e68e3d473ddc530d308
Deivid Veloso <veloso.deivid@gmail.com>
2018-10-12 17:53:58 -0300

    add signin mehod and message method

--- warbler/warbler-server/handlers/auth.js
+++ warbler/warbler-server/handlers/auth.js
@@ -1,7 +1,42 @@
 const db = require("../models");
 const jwt = require("jsonwebtoken");
 
-exports.signin = function() {};
+exports.signin = async function(req, res, next) {
+  try {
+    let user = await db.User.findOne({ email: req.body.email });
+    //busco o usuário no banco
+    let { id, username, profileImageUrl } = user;
+    //Comparo se a senha que ele informou bate com a que está no banco.
+    let isMatch = await user.comparePassword(req.body.password);
+    if (isMatch) {
+      let token = jwt.sign(
+        {
+          id,
+          username,
+          profileImageUrl
+        },
+        process.env.SECRET_KEY
+      );
+
+      return res.status(200).json({
+        id,
+        username,
+        profileImageUrl,
+        token
+      });
+    } else {
+      return next({
+        status: 500,
+        message: "Invalid Email/Password"
+      });
+    }
+  } catch (error) {
+    return next({
+      status: 700,
+      message: "Invalid Email/Password"
+    });
+  }
+};
 
 exports.signup = async function(req, res, next) {
   try {
